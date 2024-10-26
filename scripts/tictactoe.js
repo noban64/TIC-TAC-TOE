47a9b92be057056f311a88e673be43f56f0bddca
Hamdi <32712674+noban64@users.noreply.github.com>
2024-10-26 14:23:35 +0100

    Visual Tic-Tac-Toe Works

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -1,97 +1,133 @@
 var gameBoard = new Array("", "", "", "", "", "", "", "", "");
+var visualGameboard = document.querySelectorAll(".board-piece");
 /*
 ("0","1","2",
 "3","4","5",
 "6","7","8")
 */
 
-// temp 
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-
-//
-
 function Player(name, marker, win = false) {
   this.name = name;
   this.marker = marker;
   this.win = win;
+  this.turn = false;
 
-  this.move = function () {
-//temp
-    // var place;
-
-    // rl.question('Where do you want to move?', (answer) => {
-    //     console.log(`You entered: ${answer}`);
-    //     place = answer;
-    //     rl.close();
-    // });
-
-    var place = prompt("Where on the board do you want to move?");
-    // catch statement to ensure they enter a number that is valid
-
-    if (place > 0 && place < 10) {
-      gameBoard[place - 1] = this.marker;
-      console.log(gameBoard);
+  this.move = function (place) {
+    if (
+      place >= 0 &&
+      place < 10 &&
+      !visualGameboard[place].classList.contains("occupied")
+    ) {
+      console.log(visualGameboard[place]);
+      visualGameboard[place].textContent = this.marker;
+      visualGameboard[place].classList.add("occupied");
+      this.turn = false;
+      return true;
+    } else if (place === null) {
+      console.log("Player cancelled the interaction!");
+      return true;
     } else {
-      this.move();
+      visualGameboard[place].classList.add("occupied");
+      console.log("You can't make this move!");
     }
   };
 }
-
-console.log(gameBoard);
 
 function game() {
   var gameWin = false;
   var currentRound = 0;
   const playerOne = new Player("Player 1", "O");
   const playerTwo = new Player("Player 2", "X");
+  console.log(playerOne, playerTwo);
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  const gameStart = () => {
-    while (gameWin == false) {
-      playerOne.move();
-      round();
-      checkWin();
-      playerTwo.move();
-      round();
-      checkWin();
+  const visualMove = () => {
+    visualGameboard.forEach((node) => {
+      node.addEventListener("click", () => {
+        if (gameWin == true) {
+          this.abort;
+        } else if (node.classList.contains("occupied")) {
+          console.log("This space is occupied");
+        } else {
+          console.log(node.getAttribute("value"));
+          if (playerOne.turn == true) {
+            playerOne.move(node.getAttribute("value"));
+            playerTwo.turn = true;
+          } else if (playerTwo.turn == true) {
+            playerTwo.move(node.getAttribute("value"));
+            playerOne.turn = true;
+          }
+          round();
+          checkWin();
+        }
+      }); // event listener
+    });
   };
+
   const round = () => {
     currentRound++;
-    return currentRound;
+    console.log(`Round:${currentRound}`);
   };
   const checkWin = () => {
-    if (
-      (gameBoard[0] === gameBoard[1] && gameBoard[2]) ||
-      (gameBoard[3] === gameBoard[4] && gameBoard[5]) ||
-      (gameBoard[6] === gameBoard[7] && gameBoard[8]) ||
-      (gameBoard[0] === gameBoard[3] && gameBoard[6]) ||
-      (gameBoard[1] === gameBoard[4] && gameBoard[7]) ||
-      (gameBoard[2] === gameBoard[5] && gameBoard[8]) ||
-      (gameBoard[0] === gameBoard[4] && gameBoard[8]) ||
-      (gameBoard[2] === gameBoard[4] && gameBoard[6])
-    ) {
+    winConditions.forEach((boardPieces) => {
+      const [pOne, pTwo, pThree] = boardPieces;
+      if (
+        visualGameboard[pOne].textContent === playerOne.marker &&
+        visualGameboard[pTwo].textContent === playerOne.marker &&
+        visualGameboard[pThree].textContent === playerOne.marker
+      ) {
+        console.log(
+          `Win found at positions ${pOne + 1},${pTwo + 1},${pThree + 1}`
+        );
+        gameWin = true;
+        playerOne.win = true;
+        console.log(`${playerOne.name} wins!`);
+      } else if (
+        visualGameboard[pOne].textContent === playerTwo.marker &&
+        visualGameboard[pTwo].textContent === playerTwo.marker &&
+        visualGameboard[pThree].textContent === playerTwo.marker
+      ) {
+        console.log(
+          `Win found at positions ${pOne + 1},${pTwo + 1},${pThree + 1}`
+        );
+        gameWin = true;
+        playerTwo.win = true;
+        console.log(`${playerTwo.name} wins!`);
+      }
+    });
+    if (currentRound > 8) {
       gameWin = true;
-      gameWon();
+      console.log("It's a tie!");
     }
+    return gameWin;
   };
-
-  const gameWon = () => {
+  const gameStart = () => {
+    if (currentRound == 0) {
+      playerOne.turn = true;
+    }
+    visualMove();
     if (gameWin == true) {
-      this.playerOne.win = true;
-      return "Someone wins!";
-    } else if (currentRound > 9) {
-      gameWin = true;
-      return "It's a tie!";
+      visualMove(); // disable movement
     }
   };
-
+  resetGame();
   gameStart();
 }
+function resetGame() {
+  gameBoard = ["", "", "", "", "", "", "", "", ""];
+  visualGameboard.forEach((node) => {
+    node.textContent = "";
+    node.classList.remove("occupied");
+  });
+  console.log(visualGameboard);
+  console.log(gameBoard);
 }
-
-game();
